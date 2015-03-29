ec020c2a3489ec2b43bf3a4779ffbb8d77871f1b
Irina Ogereleva <irinaogereleva@gmail.com>
2015-03-30 00:25:21 +0300

    all project

--- js/script.js
+++ js/script.js
@@ -1,22 +1,11 @@
 $(document).ready(function(){
-
-    $('#scene').parallax();
-
-    $('#nextsection').on('click', function(){
-		$target = $($(this).attr('href')).offset().top-30;
-		return false;
-	});
-
      
-    $("#gallery-slider").owlCarousel({
+    $("#review-slider").owlCarousel({
 	    autoPlay: 3000,
 	    navigation: true,
 	    slideSpeed: 400,
 	     
-	    items : 4,
-	    itemsDesktop : [1080,4],
-	    itemsTablet: [718,1],
-	    itemsMobile: false
+	    items: 3
     });
 
 });
